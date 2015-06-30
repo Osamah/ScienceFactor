60394609067e7b65c5a9384bda868093a17e08ad
Osamah <osamah.bukraa@gmail.com>
2015-06-30 20:55:56 +0100

    Added rear camera support & video overlay

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 var errorCallback = function(e) {
-	console.log('Reeeejected!', e);
+	console.log('getUserMedia error ', e);
 };
 var success = function(stream) {
 	video.src = window.URL.createObjectURL(stream);
-	console.log('streaming');
+	console.log('getUserMedia success');
 }
 
 navigator.getUserMedia  = navigator.getUserMedia ||
@@ -11,10 +11,36 @@ navigator.webkitGetUserMedia ||
 navigator.mozGetUserMedia ||
 navigator.msGetUserMedia;
 
-var video = document.querySelector('video');
+var video = document.querySelector('video'),
+rearCameraId;
+
+if (MediaStreamTrack && MediaStreamTrack.getSources) {
+	MediaStreamTrack.getSources(function (sources) {
+		var rearCameraIds = sources.filter(function (source) {
+			return (source.kind === 'video' && source.facing === 'environment');
+		}).map(function (source) {
+			return source.id;
+		});
+
+		if (rearCameraIds.length) {
+			rearCameraId = rearCameraIds[0];
+		} else {
+			console.log('No rear camera found');
+		}
+	});
+} else {
+	console.log('MediaStreamTrack not supported');
+}
+
 
 if (navigator.getUserMedia) {
-	navigator.getUserMedia({ video: true }, success, errorCallback);
+	navigator.getUserMedia({
+		video: {
+			optional: [{
+				sourceId: rearCameraId
+			}]
+		}
+	}, success, errorCallback);
 } else {
-	console.log('nope');
+	console.log('navigator.getUserMedia doesnt exist');
 }
